682f40d3115778ec035ef00e876b13f94126dbe9
mojtabaabedi81 <mojtaba.abedi1381@gmail.com>
2023-10-22 15:06:48 +0330

    migration

--- database/migration.php
+++ database/migration.php
@@ -24,15 +24,15 @@ function create_inventory_table($query)
 {
     global $conn;
 
-    $query = "CREATE TABLE IF NOT EXISTS `inventory_table` (
-  `product_id` int NOT NULL AUTO_INCREMENT,
-  `product_name` varchar(256) NOT NULL,
-  `product_category` varchar(256) NOT NULL,
-  `product_quantity` varchar(256) NOT NULL,
-  `product_price` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `inserted_at` datetime NOT NULL,
-  PRIMARY KEY (`product_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=257 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
+    $query = "CREATE TABLE inventory_table (
+    id int NOT NULL AUTO_INCREMENT,
+    productID text,
+    productName varchar(256),
+    category int,
+    quantity int,
+    price datetime,
+    primary key (id)
+    )";
 
     return $conn->query($query);
 }
@@ -44,11 +44,11 @@ function createTableIfNotExist()
     $result = $conn->query("SHOW TABLES LIKE 'users'");
     if ($result-> rowCount() == 0){
 
-        create_users_table($query);
+        create_users_table();
     }
     $result = $conn->query("SHOW TABLES LIKE 'inventory_table'");
     if ($result->rowCount() == 0 ){
-        create_inventory_table($query);
+        create_inventory_table();
     }
 
 }
