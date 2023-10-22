6b32a4a322e5a03b4350c64e4c5c537099ae3f99
mojtabaabedi81 <mojtaba.abedi1381@gmail.com>
2023-10-22 11:58:01 +0330

    make the router dynamic for wamp and xammp

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//error_reporting(E_ALL & ~E_WARNING);
+error_reporting(E_ALL & ~E_WARNING);
 require_once __DIR__ . "/database/database.php";
 require_once __DIR__ . "/helper/helper.php";
 require_once __DIR__ . "/config/config.php";
@@ -8,27 +8,42 @@ require_once __DIR__ . "/database/migration.php";
 
 createTableIfNotExist();
 
-$url = trim($_SERVER['REQUEST_URI'], '/');
-//$path = !empty($url) ? $url : "user/login";
-if (!empty($url)){
-    $path = "user/login";
-}else{
-    $path = $url;
+
+
+if (isset($_SERVER['REQUEST_URI'])) {
+    $url = trim($_SERVER['REQUEST_URI'], '/');
+    $path = !empty($url) ? $url : "user/login";
+
+    $query_params = explode('?', $path);
+
+
+    if (!empty($query_params[1])) {
+        $path = explode('?', $path);
+    }
+    list($controller, $method) = explode('/', $path);
+
+    $file = __DIR__ . "/controller/{$controller}_controller.php";
+    if (file_exists($file)) {
+        require $file;
+        $method($_REQUEST);
+    } else {
+        $server_path = getcwd();
+        $root_name = basename($server_path);
+
+
+        list($controller, $method) = explode('/', $server_path);
+        $file = __DIR__ . "/controller/user_controller.php";
+        $method = "login";
+
+        if (file_exists($file)) {
+            require $file;
+            $method($_REQUEST);
+        } else {
+            die('something went wrong');
+        }
+
+    }
 }
-$query_params = explode('?', $path);
 
 
-if (!empty($query_params[1])) {
-    $path = explode('?', $path);
-}
 
-list($controller, $method) = explode('/', $path);
-
-$file = __DIR__ . "/controller/{$controller}_controller.php";
-
-if (file_exists($file)) {
-    require $file;
-    $method($_REQUEST);
-} else {
-    die('something went wrong');
-}
