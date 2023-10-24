9a7ce91fb0ccf0fa850f4c110f433bf321ed5553
mojtabaabedi81 <mojtaba.abedi1381@gmail.com>
2023-10-24 12:41:43 +0330

    set validation for email and password

--- controller/user_controller.php
+++ controller/user_controller.php
@@ -3,54 +3,66 @@
 function login()
 {
     if (post('loginRequest')) {
-            $email = post('email');
-            $password = post('password');
+        $email = post('email');
+        $password = post('password');
 
-            if (user_exists($email)) {
-                $login = user_login($email, $password);
-                if ($login) {
-                    $_SESSION['user_email'] = $email;
-                    header("Location:../inventoryTable/show");
-                    exit();
+
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            if (strlen($password) >= 6) {
+                if (user_exists($email)) {
+                    $login = user_login($email, $password);
+                    if ($login) {
+                        $_SESSION['user_email'] = $email;
+                        header("Location:../inventoryTable/show");
+                        exit();
+                    } else {
+                        echo "User or password is incorrect!";
+                    }
                 } else {
-                    echo "user or password wrong !";
+                    echo "User not found!";
                 }
-
-            } else
-                echo "user not found !";
-        } else
-            view("loginForm");
-
+            } else {
+                echo "Password must be at least 6 characters long.";
+            }
+        } else {
+            echo "Invalid email address!";
+        }
+    } else {
+        view("loginForm");
+    }
 }
 
 
 function register()
 {
     $email = post('email');
+    $password = post('password');
 
     if (post('email')) {
-        $email = post('email');
-        $password = post('password');
-
-        if (empty($email) || empty($password)) {
-            echo "Please fill in all required fields.";
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            if (strlen($password) >= 6) {
+                if (empty($email) || empty($password)) {
+                    echo "Please fill in all required fields.";
+                } else {
+                    if (!user_exists($email)) {
+                        create_user($email, md5($password));
+                        header("Location: ../inventoryTable/show");
+                    } else {
+                        echo "This account already exists.";
+                    }
+                }
+            } else {
+                echo "Password must be at least 6 characters long.";
+            }
         } else {
-
-            if (!user_exists($email)) {
-                create_user($email, md5($password));
-
-                header("Location: ../inventoryTable/show");
-
-            } else
-                echo "this account already exist";
+            echo "Invalid email address.";
         }
     }
-
 }
 
 
-function logout ()
+function logout()
 {
     session_destroy();
-    header('Location: ' . BASEURL . '/user/login');
+    header('Location: ' . BASEURL);
 }
