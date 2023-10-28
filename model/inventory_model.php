2d421582aa5c42b6a28a42ada4444765815bdbeb
mojtabaabedi81 <mojtaba.abedi1381@gmail.com>
2023-10-28 15:52:08 +0330

    make the function for get the product list

--- model/inventory_model.php
+++ model/inventory_model.php
@@ -1,6 +1,6 @@
 <?php
 
-function create_product($user_id,$product_name, $product_no, $product_category, $product_quantity, $product_price): bool
+function create_product($user_id, $product_name, $product_no, $product_category, $product_quantity, $product_price): bool
 {
     global $conn;
 
@@ -66,4 +66,23 @@ function edit_product($product_no, $product_name, $product_category, $product_qu
 
     return $stmt->execute();
 
+}
+
+function get_product_list($user_id, $sheet)
+{
+    global $conn;
+    $query = "SELECT * FROM inventory_table WHERE user_id = $user_id";
+    $stmt = $conn->query($query);
+    $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $rowCount = 2;
+    foreach ($row as $item) {
+        $sheet->setCellValue('C' . $rowCount, 'product_no  :' . $item['product_no']);
+        $sheet->setCellValue('D' . $rowCount, 'product_name :' . $item['product_name']);
+        $sheet->setCellValue('E' . $rowCount, 'product_category :' . $item['product_category']);
+        $sheet->setCellValue('F' . $rowCount, 'product_quantity :' . $item['product_quantity']);
+        $sheet->setCellValue('G' . $rowCount, 'created_at :' . $item['created_at']);
+        $rowCount++;
+
+
+    }
 }
